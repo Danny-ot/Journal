d1ac1b840048b82c57e0d872a72df79c4acef959
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-03-03 01:30:14 -0800

    Create the method for the wordcounter for the entry constructor

--- src/journal.js
+++ src/journal.js
@@ -10,3 +10,16 @@ export function Entry(title , body){
     this.title = title;
     this.body = body;
 }
+
+// Method For Counting the word
+Entry.prototype.countWord = function(){
+    let counter = 0;
+    const wordArray = this.body.split(" ");
+    if(this.body.trim() === ""){
+        return 0 ;
+    }
+    wordArray.forEach(function(word){
+        counter ++;
+    })
+    return counter
+};
