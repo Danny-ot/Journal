8548a35715f1fb2e9aa17788af280be7e2ac1bbb
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-03-03 02:51:13 -0800

    Create the method for assigning id

--- src/journal.js
+++ src/journal.js
@@ -1,41 +1,48 @@
 
 // Journal Constructor
-export function Journal(){
+export function Journal() {
     this.entries = {
 
     };
+    this.id = 0;
 }
 
+// Method For Assigning Id
+Journal.prototype.assignId = function () {
+    this.id += 1;
+    return this.id;
+};
+
 // Entries Constructor
-export function Entry(title , body){
+export function Entry(title, body) {
     this.title = title;
     this.body = body;
 }
 
 // Method For Counting the word
-Entry.prototype.countWord = function(){
-    if(this.body.trim() === ""){
-        return 0 ;
+Entry.prototype.countWord = function () {
+    if (this.body.trim() === "") {
+        return 0;
     }
     let counter = 0;
     const wordArray = this.body.split(" ");
-    wordArray.forEach(function(){
-        counter ++;
+    wordArray.forEach(function () {
+        counter++;
     });
     return counter;
 };
 
 // Method For Counting Vowels
-Entry.prototype.countVowel = function(){
-    if(this.body.trim() === ""){
-        return 0 ;
+Entry.prototype.countVowel = function () {
+    if (this.body.trim() === "") {
+        return 0;
     }
-    const vowels = ['a' , 'e' , 'i' , 'o' , 'u'];
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
     const letterArray = this.body.split("");
     let counter = 0;
-    letterArray.forEach(function(elements){
-        if(vowels.includes(elements.toLowerCase())){
-            counter ++;
+    letterArray.forEach(function (elements) {
+        if (vowels.includes(elements.toLowerCase())) {
+            counter++;
             return counter;
         }
     });
@@ -43,18 +50,18 @@ Entry.prototype.countVowel = function(){
 };
 
 // Method For Countimg Consonant
-Entry.prototype.countConsonant = function(){
-    if(this.body.trim() === ""){
-        return 0 ;
+Entry.prototype.countConsonant = function () {
+    if (this.body.trim() === "") {
+        return 0;
     }
-    const vowels = ["a" , "e" , "i" , "O" , "u"];
+    const vowels = ["a", "e", "i", "O", "u"];
     const letterArray = this.body.split("");
     let counter = 0;
-    letterArray.forEach(function(elements){
-        if(vowels.includes(elements.toLowerCase())){
-           return 0 ;
-        }else{
-            counter ++;
+    letterArray.forEach(function (elements) {
+        if (vowels.includes(elements.toLowerCase())) {
+            return 0;
+        } else {
+            counter++;
         }
 
     });
@@ -62,18 +69,18 @@ Entry.prototype.countConsonant = function(){
 };
 
 // Method For Getting Teaser
-Entry.prototype.getTeaser = function(){
-    if(this.body.trim() === ""){
-        return "" ;
+Entry.prototype.getTeaser = function () {
+    if (this.body.trim() === "") {
+        return "";
     }
     const sentenceSplit = this.body.split(".");
     let firstSentenceSplit = sentenceSplit[0].split(" ");
-    if(firstSentenceSplit.length > 8){
-         firstSentenceSplit = firstSentenceSplit.slice(0 , 9);
+    if (firstSentenceSplit.length > 8) {
+        firstSentenceSplit = firstSentenceSplit.slice(0, 9);
     }
-    
+
     let sentence = firstSentenceSplit.join(" ");
-    
+
     return sentence.concat(".....");
 }
 
