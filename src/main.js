0d8112ba8e38083b73cf6c5497d86ab1f60c497e
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-03-03 15:26:30 -0800

    Create the attach listener function to attach action click listener

--- src/main.js
+++ src/main.js
@@ -7,17 +7,32 @@ import { Entry } from './journal.js';
 
 let journal = new Journal();
 
-// Display Content Function
 function displayContent(journal) {
     let displayDiv = $("#display-list");
     let htmlString = "";
     Object.keys(journal.entries).forEach(function (key) {
         let entry = journal.findEntry(key);
-        htmlString += "<div><h3>" + entry.title + "</h3><p>" + entry.getTeaser() + "<p></div>";
+        htmlString += "<div class = 'mt-4 '><h4>" + entry.title + "</h4><p class ='entry-body' id = " + entry.id + ">" + entry.getTeaser() + "<p></div>";
     })
     displayDiv.html(htmlString)
 }
 
+
+// AttachListener Function
+function attachListener(){
+    $("div#display-list").on("click" , "p" , function(){
+        showBodyContent(this.id);
+    })
+}
+
+// ShoWBodyContent Function 
+function showBodyContent(id){
+    let entry = journal.findEntry(id);
+    $("#word-amount").html(entry.countWord());
+    $("#vowel-amount").html(entry.countVowel());
+    $("#conso-amount").html(entry.countConsonant());
+    $("#" + id).html(entry.body)
+}
 
 attachListener();
 $("#form").submit(function (e) {
